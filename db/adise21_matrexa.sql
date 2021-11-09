26d131f788407d3c2ad6cf235ba8754cf9fff7af
Μαυρίδης Στάθης 174871 <51161382+stathis39@users.noreply.github.com>
2021-11-09 22:54:38 +0200

    Update adise21_matrexa.sql

--- db/adise21_matrexa.sql
+++ db/adise21_matrexa.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 09, 2021 at 09:52 PM
+-- Generation Time: Nov 09, 2021 at 09:54 PM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 7.3.31
 
@@ -20,6 +20,43 @@ SET time_zone = "+00:00";
 --
 -- Database: `adise21_matrexa`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `board`
+--
+
+CREATE TABLE `board` (
+  `x` tinyint(1) NOT NULL,
+  `y` tinyint(1) NOT NULL,
+  `piece_color` enum('W','B') DEFAULT NULL,
+  `piece_height` enum('S','T') DEFAULT NULL,
+  `piece_consistency` enum('H','SO') DEFAULT NULL,
+  `piece_shape` enum('SQ','R') DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `board`
+--
+
+INSERT INTO `board` (`x`, `y`, `piece_color`, `piece_height`, `piece_consistency`, `piece_shape`) VALUES
+(1, 1, NULL, NULL, NULL, NULL),
+(1, 2, NULL, NULL, NULL, NULL),
+(1, 3, NULL, NULL, NULL, NULL),
+(1, 4, NULL, NULL, NULL, NULL),
+(2, 1, NULL, NULL, NULL, NULL),
+(2, 2, NULL, NULL, NULL, NULL),
+(2, 3, NULL, NULL, NULL, NULL),
+(2, 4, NULL, NULL, NULL, NULL),
+(3, 1, NULL, NULL, NULL, NULL),
+(3, 2, NULL, NULL, NULL, NULL),
+(3, 3, NULL, NULL, NULL, NULL),
+(3, 4, NULL, NULL, NULL, NULL),
+(4, 1, NULL, NULL, NULL, NULL),
+(4, 2, NULL, NULL, NULL, NULL),
+(4, 3, NULL, NULL, NULL, NULL),
+(4, 4, NULL, NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -58,9 +95,87 @@ INSERT INTO `board_empty` (`x`, `y`, `piece_color`, `piece_height`, `piece_consi
 (4, 3, NULL, NULL, NULL, NULL),
 (4, 4, NULL, NULL, NULL, NULL);
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `game_status`
+--
+
+CREATE TABLE `game_status` (
+  `status` enum('not active','initialized','started','ended','aborded') NOT NULL DEFAULT 'not active',
+  `p_turn` enum('P1','P2') DEFAULT NULL,
+  `result` enum('P1','P2','D') DEFAULT NULL,
+  `last_change` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Triggers `game_status`
+--
+DELIMITER $$
+CREATE TRIGGER `game_status_update` BEFORE UPDATE ON `game_status` FOR EACH ROW BEGIN
+	SET NEW.last_change=NOW();
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pieces`
+--
+
+CREATE TABLE `pieces` (
+  `color` varchar(255) COLLATE utf8_bin NOT NULL,
+  `height` varchar(255) COLLATE utf8_bin NOT NULL,
+  `consistency` varchar(255) COLLATE utf8_bin NOT NULL,
+  `Shape` varchar(255) COLLATE utf8_bin NOT NULL,
+  `id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Dumping data for table `pieces`
+--
+
+INSERT INTO `pieces` (`color`, `height`, `consistency`, `Shape`, `id`) VALUES
+('black', 'tall', 'hollow', 'square', 1),
+('black', 'tall', 'hollow', 'round', 2),
+('black', 'tall', 'solid', 'square', 3),
+('black', 'tall', 'solid', 'round', 4),
+('black', 'short', 'hollow', 'square', 5),
+('black', 'short', 'hollow', 'round', 6),
+('black', 'short', 'solid', 'square', 7),
+('black', 'short', 'solid', 'round', 8),
+('white', 'tall', 'hollow', 'square', 9),
+('white', 'tall', 'hollow', 'round', 10),
+('white', 'tall', 'solid', 'square', 11),
+('white', 'tall', 'solid', 'round', 12),
+('white', 'short', 'hollow', 'square', 13),
+('white', 'short', 'hollow', 'round', 14),
+('white', 'short', 'solid', 'square', 15),
+('white', 'short', 'solid', 'round', 16);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `players`
+--
+
+CREATE TABLE `players` (
+  `username` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `Player_n` enum('p1','p2') COLLATE utf8_bin NOT NULL,
+  `Token` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `last_action` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `board`
+--
+ALTER TABLE `board`
+  ADD PRIMARY KEY (`x`,`y`);
 
 --
 -- Indexes for table `board_empty`
