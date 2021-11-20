e5af18635feb09d715e3ac2be3f9f0315a04adef
Μαυρίδης Στάθης 174871 <51161382+stathis39@users.noreply.github.com>
2021-11-20 19:06:00 +0200

    create draw_empty_board function

--- js/quatrojs.js
+++ js/quatrojs.js
@@ -1,6 +1,8 @@
 $(function () {
   draw_board_with_pieces();
+  draw_empty_board();
   fill_board();
+  create_board_by_data();
   fill_board_by_data();
 
 
@@ -9,41 +11,6 @@ function generate_table() {
 
   // get the reference for the body
   document.getElementById('btn').style.visibility = 'hidden';
-
-
-
-  var body = document.getElementsByTagName("body")[0];
-
-  // creates a <table> element and a <tbody> element
-
-  var tbl = document.createElement("table");
-  var tblBody = document.createElement("tbody");
-
-  // creating all cells
-  for (var i = 1; i < 5; i++) {
-    // creates a table row
-    var row = document.createElement("tr");
-
-    for (var j = 1; j < 5; j++) {
-      // Create a <td> element and a text node, make the text
-      // node the contents of the <td>, and put the <td> at
-      // the end of the table row
-      var cell = document.createElement("td");
-      var cellText = document.createTextNode(i + ',' + j);
-      cell.appendChild(cellText);
-      row.appendChild(cell);
-    }
-
-    // add the row to the end of the table body
-    tblBody.appendChild(row);
-  }
-
-  // put the <tbody> in the <table>
-  tbl.appendChild(tblBody);
-  // appends <table> into <body>
-  body.appendChild(tbl);
-  // sets the border attribute of tbl to 2;
-  tbl.setAttribute("border", "2");
 }
 //takes the json file from the url 
 
@@ -59,6 +26,19 @@ function draw_board_with_pieces() {
   t += '</table>';
   $('#quarto_board').html(t);
 }
+function draw_empty_board() {
+  var t = '<table id="board_table">';
+  for (var i = 1; i < 5; i++) {
+    t += '<tr>';
+    for (var j = 1; j < 5; j++) {
+      t += '<td class="guarto_square" id="square' + i + '_' + j + '">' + i + ',' + j + '</td>';
+    }
+    t += '</tr>';
+  }
+  t += '</table>';
+  $('#board').html(t);
+}
+
 function fill_board() {
   $.ajax({
     url: "quatro.php/pieces",
@@ -66,14 +46,31 @@ function fill_board() {
     dataType: "json",
     success: fill_board_by_data
   });
+  $.ajax({
+    url: "quatro.php/board",
+    type: "GET",
+    dataType: "json",
+    success: create_board_by_data
+  });
+}
+function create_board_by_data(data) {
+  for (var i = 0; i < 16; i++) {
+    var o = data[i];
+    var id = '#square' + o.x + '_' + o.y;
+    $(id).html(id);
+  }
 }
 function fill_board_by_data(data) {
 
-  for (var i = 0; i < data.length; i++) {
+  for (var i = 0; i < 16; i++) {
     var o = data[i];
     var id = '#square_' + o.id;
     var c = (o.id != null) ? o.id : '';
     var im = (o.id != null) ? '<img class="piece" src="images/' + c + '.jpg">' : '';
     $(id).html(im);
   }
+
 }
+
+
+
